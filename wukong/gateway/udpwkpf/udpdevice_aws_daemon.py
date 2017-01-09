8c23db84fe1b206cf4836c53c77a3d387442bf73
hyww <b02902009@ntu.edu.tw>
2017-01-09 14:02:27 +0800

    aws_daemon: add property dataNo and -t test mode

--- wukong/gateway/udpwkpf/udpdevice_aws_daemon.py
+++ wukong/gateway/udpwkpf/udpdevice_aws_daemon.py
@@ -13,8 +13,9 @@ if __name__ == "__main__":
             WuClass.__init__(self)
             self.loadClass('AWS_Daemon')
             self.id = 0
+            self.dataNo = 0;
             self.myMQTTClient = AWSIoTMQTTClient("")
-            self.myMQTTClient.configureEndpoint("a1trumz0n7avwt.iot.us-west-2.amazonaws.com", 8883)
+            self.myMQTTClient.configureEndpoint("a13mxqj7o85rqq.iot.ap-northeast-1.amazonaws.com",8883)
             self.myMQTTClient.configureCredentials("AWS/root.crt", "AWS/private.key", "AWS/cert.crt")
             self.myMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
             self.myMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
@@ -31,8 +32,8 @@ if __name__ == "__main__":
             print("--------------\n\n")
             try:
                 data = json.loads(message.payload)
-                if 'fire' in data:
-                    self.obj.setProperty(pID, data['fire'])
+                if 'data' in data and sys.argv[1] != '-t':
+                    self.obj.setProperty(pID, data['data'])
             except ValueError, e:
                 print 'not JSON'
 
@@ -43,21 +44,22 @@ if __name__ == "__main__":
             self.Callback(client, userdata, message, 2)
 
         def update(self,obj,pID=None,val=None):
-            if self.id != (int)(obj.getProperty(0)):
+            if self.id != (int)(obj.getProperty(0)) or self.dataNo != (int)(obj.getProperty(6)):
                 self.id = (int)(obj.getProperty(0))
+                self.dataNo = (int)(obj.getProperty(6))
                 self.obj = obj
-                print self.id
-                self.myMQTTClient.subscribe('Device/'+str(self.id)+'/L/Recv', 1, self.LCallback)
-                self.myMQTTClient.subscribe('Device/'+str(self.id)+'/R/Recv', 1, self.RCallback)
-            
+                print self.id, ",", self.dataNo
+                self.myMQTTClient.subscribe('Device/'+str(self.id)+'/L/Recv/'+str(self.dataNo), 1, self.LCallback)
+                self.myMQTTClient.subscribe('Device/'+str(self.id)+'/R/Recv/'+str(self.dataNo), 1, self.RCallback)
+
             if pID == 3:
                 print pID
-                mess = json.dumps({'fire': val})
-                self.myMQTTClient.publish('Device/'+str(self.id-1)+'/R/Recv', mess, 0)
+                msg = json.dumps({'data': val})
+                self.myMQTTClient.publish('Device/'+str(self.id-1)+'/R/Recv/'+str(self.dataNo), msg, 0)
             if pID == 4:
                 print pID
-                mess = json.dumps({'fire': val})
-                self.myMQTTClient.publish('Device/'+str(self.id+1)+'/L/Recv', mess, 0)
+                msg = json.dumps({'data': val})
+                self.myMQTTClient.publish('Device/'+str(self.id+1)+'/L/Recv/'+str(self.dataNo), msg, 0)
 
     class MyDevice(Device):
         def __init__(self,addr,localaddr):
@@ -67,8 +69,15 @@ if __name__ == "__main__":
             self.m = AWS_Daemon()
             self.addClass(self.m,0)
             self.obj_philip = self.addObject(self.m.ID)
-
-    if len(sys.argv) <= 2:
+    if sys.argv[1] == '-t':
+        a = AWS_Daemon()
+        a.id = (int)(sys.argv[2])
+        a.dataNo = (int)(sys.argv[3])
+	a.myMQTTClient.subscribe('Device/'+str(a.id)+'/L/Recv/'+str(a.dataNo), 1, a.LCallback)
+	a.myMQTTClient.subscribe('Device/'+str(a.id)+'/R/Recv/'+str(a.dataNo), 1, a.RCallback)
+	reactor.run()
+     
+    elif len(sys.argv) <= 2:
         print 'python %s <gip> <dip>:<port>' % sys.argv[0]
         print '      <gip>: IP addrees of gateway'
         print '      <dip>: IP address of Python device'
@@ -76,5 +85,6 @@ if __name__ == "__main__":
         print ' ex. python %s 192.168.4.7 127.0.0.1:3000' % sys.argv[0]
         sys.exit(-1)
 
-    d = MyDevice(sys.argv[1],sys.argv[2])
-    reactor.run()
+    else:   
+	d = MyDevice(sys.argv[1],sys.argv[2])
+	reactor.run()
